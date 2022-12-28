815d9027fe9489585c4425338af9c2c28e888f7b
ahosanhabib786 <mdahosanhabib786@gmail.com>
2022-12-28 16:05:28 +0600

    css grid system js

--- index.js
+++ index.js
@@ -25,18 +25,39 @@ function createCard(cardItem){
 
     //add class to inner card element
     addClassToElement(cardInnerElem ,'card-inner')
+
     //add class to front card element
     addClassToElement(cardForntElem ,'card-fornt')
+
     //add class to Back card element
     addClassToElement(cardBackElem ,'card-back')
+
     //add Src attribute and appropiate value to img Element Back of card
     addSrcToImgageElem(cardBackElem , cardBackImgPath)
-     //add Src attribute and appropiate value to img Element front of card
-     addSrcToImgageElem(cardBackElem , cardItem.imagePath)
-     //asign class to back Image element to back of card
-     addClassToElement(cardBackElem ,'card-img')
-          //asign class to back Image element to fornt of card
-     addClassToElement(cardForntElem ,'card-img')
+
+    //add Src attribute and appropiate value to img Element front of card
+    addSrcToImgageElem(cardBackElem , cardItem.imagePath)
+
+    //asign class to back Image element to back of card
+    addClassToElement(cardBackElem ,'card-img')
+
+    //asign class to back Image element to fornt of card
+    addClassToElement(cardForntElem ,'card-img')
+
+    //add front image element as a child element to a front card elment
+    addChildElement(cardFrontElem ,cardFrontImg)
+
+    //add back image element as a child element to a back card elment
+    addChildElement(cardBackElem ,cardBackImg)
+
+    //add front image element as a child element to a front card elment
+    addChildElement(cardInnerElem , cardFrontElem)
+
+    //add back image element as a child element to a back card elment
+    addChildElement(cardInnerElem , cardBackElem)
+
+    //add inner element as a child element to  card elment
+    addChildElement(cardElem , cardInnerElem)
 
 
 
@@ -54,4 +75,8 @@ function addIdToElement( elem, Id)
 }
 function addSrcToImgElement(elem, src){
     imgElem.src =src
+}
+function addChildElement(parentElem , childElem)
+{
+    parentElem.appendChild(childElem)
 }
